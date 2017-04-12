fd586ea532ac5945182ac89033df29294e9b1d32
Steve Haslam <araqnid@googlemail.com>
2017-04-12 02:21:54 +0100

    Drop need for jquery in main.js itself

--- ui/src/main/web/main.jsx
+++ ui/src/main/web/main.jsx
@@ -1,11 +1,11 @@
-define(['jquery', 'react', 'react-dom',
+define(['react', 'react-dom',
         'app/Status', 'app/Bus', 'app/status.css'],
-function status$$init($, React, ReactDOM,
+function status$$init(React, ReactDOM,
                 Status, Bus) {
     window.BUS = new Bus();
-    const $elt = $("<div id='component.Status'></div>");
-    $elt.appendTo(document.body);
-    const elt = $elt[0];
+    const elt = document.createElement("div");
+    elt.id = "component.Status";
+    document.body.insertBefore(elt, document.body.childNodes[0]);
     ReactDOM.render(<Status/>, elt);
     return "status";
 });
