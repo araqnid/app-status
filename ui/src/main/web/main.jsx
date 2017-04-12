e85a5b91c9ab5b176225ae3c46b54e348f282507
Steve Haslam <araqnid@googlemail.com>
2017-04-12 02:44:30 +0100

    Move from AMD modules to ES-import modules

--- ui/src/main/web/main.jsx
+++ ui/src/main/web/main.jsx
@@ -1,11 +1,11 @@
-define(['react', 'react-dom',
-        'app/Status', 'app/Bus', 'app/status.css'],
-function status$$init(React, ReactDOM,
-                Status, Bus) {
-    window.BUS = new Bus();
-    const elt = document.createElement("div");
-    elt.id = "component.Status";
-    document.body.insertBefore(elt, document.body.childNodes[0]);
-    ReactDOM.render(<Status/>, elt);
-    return "status";
-});
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Status from 'app/Status';
+import Bus from 'app/Bus';
+import css from 'app/status.css';
+
+window.BUS = new Bus();
+const elt = document.createElement("div");
+elt.id = "component.Status";
+document.body.insertBefore(elt, document.body.childNodes[0]);
+ReactDOM.render(<Status/>, elt);
