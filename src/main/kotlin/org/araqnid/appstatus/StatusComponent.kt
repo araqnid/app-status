0c252e5661f2392208b95fcfdf73b82aaadb959e
Steve Haslam <araqnid@googlemail.com>
2017-11-29 04:00:49 +0000

    Various methods for manipulating status components and their values

--- src/main/kotlin/org/araqnid/appstatus/StatusComponent.kt
+++ src/main/kotlin/org/araqnid/appstatus/StatusComponent.kt
@@ -5,6 +5,21 @@ import java.util.function.Supplier
 abstract class StatusComponent(val id: String, val label: String) {
     abstract fun report(): StatusReport
 
+    fun mapReport(fn: (StatusReport) -> StatusReport): StatusComponent = object : StatusComponent(id, label) {
+        override fun report() = fn(this@StatusComponent.report())
+        override fun toString() = this@StatusComponent.toString()
+    }
+
+    fun mapText(fn: (String) -> String): StatusComponent = mapReport { it.mapText(fn) }
+    fun limitPriority(maxPriority: StatusReport.Priority): StatusComponent = mapReport { it.limitPriority(maxPriority) }
+    fun pending() = object : StatusComponent(id, "$label (Pending)") {
+        override fun report(): StatusReport {
+            val underlying = this@StatusComponent.report()
+            return StatusReport(StatusReport.Priority.INFO, "${underlying.text} (pending; priority was ${underlying.priority})")
+        }
+        override fun toString() = "${this@StatusComponent} (Pending)"
+    }
+
     companion object {
         @JvmStatic fun info(id: String, label: String, content: String) = info(id, label, { content })
         @JvmStatic fun info(id: String, label: String, supplier: Supplier<String>) = info(id, label, supplier::get)
--- src/main/kotlin/org/araqnid/appstatus/StatusReport.kt
+++ src/main/kotlin/org/araqnid/appstatus/StatusReport.kt
@@ -12,4 +12,9 @@ data class StatusReport(val priority: Priority, val text: String) {
             @JvmStatic fun higher(l: Priority, r: Priority): Priority = if (l > r) l else r
         }
     }
+
+    fun mapText(fn: (String) -> String) = copy(text = fn(text))
+    fun limitPriority(maxPriority: Priority) = if (priority > maxPriority) copy(priority = maxPriority) else this
+
+    operator fun plus(moreText: String) = copy(text = text + moreText)
 }
