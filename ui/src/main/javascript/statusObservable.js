860483d4fef8e94312ea8920d4e83c50728ab321
Steve Haslam <araqnid@googlemail.com>
2018-10-28 18:56:10 +0000

    Rephrase asActions as higher-order Observable

--- ui/src/main/javascript/statusObservable.js
+++ ui/src/main/javascript/statusObservable.js
@@ -7,18 +7,20 @@ const statusAjax = ajax.get("/_api/info/status", accept("application/json"));
 const versionAjax = ajax.get("/_api/info/version", accept("application/json"));
 const readinessAjax = ajax.get("/_api/info/readiness", accept("text/plain"));
 
-function asActions(observable, type) {
-    return new Observable(observer => {
-        observable.subscribe(
-            value => { observer.next({ type: type, payload: value }) },
-            error => { observer.next({ type: `${type}.error`, payload: error, error: true }); observer.complete() },
-            () => { observer.complete() }
-        );
-    });
+export function asActions(type) {
+    return underlying => {
+        return new Observable(observer => {
+            underlying.subscribe(
+                value => { observer.next({ type: type, payload: value }) },
+                error => { observer.next({ type: `${type}.error`, payload: error, error: true }); observer.complete() },
+                () => { observer.complete() }
+            );
+        });
+    };
 }
 
-const statusAsActions = asActions(statusAjax, "status");
-const versionAsActions = asActions(versionAjax, "version");
-const readinessAsActions = asActions(readinessAjax, "readiness");
+const statusAsActions = asActions("status")(statusAjax);
+const versionAsActions = asActions("version")(versionAjax);
+const readinessAsActions = asActions("readiness")(readinessAjax);
 
 export const loadStatus = merge(statusAsActions, versionAsActions, readinessAsActions);
