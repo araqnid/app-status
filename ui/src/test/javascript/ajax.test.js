6894c66da5ed2d3d01852d7103558cc2f36b5353
Steve Haslam <araqnid@googlemail.com>
2019-09-14 09:55:42 +0100

    Remove top-level describes

--- ui/src/test/javascript/ajax.test.js
+++ ui/src/test/javascript/ajax.test.js
@@ -4,40 +4,38 @@ import * as ajax from "../../main/javascript/ajax";
 import {localAxios} from "../../main/javascript/ajax";
 import {asActions} from "../../main/javascript/observables";
 
-describe("ajax observables", () => {
-    let mock = null;
+let mock = null;
 
-    beforeAll(() => {
-        mock = new MockAdapter(localAxios);
+beforeAll(() => {
+    mock = new MockAdapter(localAxios);
+});
+
+afterAll(() => {
+    mock.restore();
+});
+
+afterEach(() => {
+    mock.reset();
+});
+
+describe("get", () => {
+    it("gets from API and returns response data", async () => {
+        mock.onGet("/test").reply(200, { data: "xyzzy" });
+        const actions = [];
+        await asActions("fetch")(ajax.get("/test")).forEach(action => actions.push(action));
+        assert.deepStrictEqual(actions, [
+            { type: "fetch", payload: { data: "xyzzy" } }
+        ]);
     });
 
-    afterAll(() => {
-        mock.restore();
-    });
-
-    afterEach(() => {
-        mock.reset();
-    });
-
-    describe("get", () => {
-        it("gets from API and returns response data", async () => {
-            mock.onGet("/test").reply(200, { data: "xyzzy" });
-            const actions = [];
-            await asActions("fetch")(ajax.get("/test")).forEach(action => actions.push(action));
-            assert.deepStrictEqual(actions, [
-                { type: "fetch", payload: { data: "xyzzy" } }
-            ]);
-        });
-
-        it("gets from API and propagates error", async () => {
-            mock.onGet("/test").reply(500);
-            const actions = [];
-            await asActions("fetch")(ajax.get("/test"))
-                .map(action => action.error ? ({ type: action.type, error: true, payload: action.payload.response.status }) : action)
-                .forEach(action => actions.push(action));
-            assert.deepStrictEqual(actions, [
-                { type: "fetch.error", payload: 500, error: true }
-            ]);
-        });
+    it("gets from API and propagates error", async () => {
+        mock.onGet("/test").reply(500);
+        const actions = [];
+        await asActions("fetch")(ajax.get("/test"))
+            .map(action => action.error ? ({ type: action.type, error: true, payload: action.payload.response.status }) : action)
+            .forEach(action => actions.push(action));
+        assert.deepStrictEqual(actions, [
+            { type: "fetch.error", payload: 500, error: true }
+        ]);
     });
 });
