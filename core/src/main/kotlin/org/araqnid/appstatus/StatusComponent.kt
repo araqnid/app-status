2a36aac19e8f2d1aaa37020193babd25fea39d0e
Steve Haslam <araqnid@googlemail.com>
2020-02-17 17:15:54 +0000

    Make StatusComponent final, taking a supplier property

--- core/src/main/kotlin/org/araqnid/appstatus/StatusComponent.kt
+++ core/src/main/kotlin/org/araqnid/appstatus/StatusComponent.kt
@@ -2,26 +2,25 @@ package org.araqnid.appstatus
 
 import java.util.function.Supplier
 
-abstract class StatusComponent(val id: String, val label: String) : ReportConfigurable<StatusComponent> {
-    abstract fun report(): StatusReport
+class StatusComponent(val id: String, val label: String, private val supplier: () -> StatusReport) : ReportConfigurable<StatusComponent> {
+    fun report(): StatusReport = supplier()
 
-    fun mapReport(fn: (StatusReport) -> StatusReport): StatusComponent = object : StatusComponent(id, label) {
-        override fun report() = fn(this@StatusComponent.report())
-        override fun toString() = this@StatusComponent.toString()
-    }
+    fun mapReport(fn: (StatusReport) -> StatusReport) =
+            StatusComponent(id, label) { fn(supplier()) }
 
-    override fun mapText(fn: (String) -> String): StatusComponent = mapReport { it.mapText(fn) }
-    override fun limitPriority(maxPriority: StatusReport.Priority): StatusComponent = mapReport { it.limitPriority(maxPriority) }
+    override fun mapText(fn: (String) -> String): StatusComponent =
+            mapReport { it.mapText(fn) }
+    override fun limitPriority(maxPriority: StatusReport.Priority): StatusComponent =
+            mapReport { it.limitPriority(maxPriority) }
 
     companion object {
         @JvmStatic fun info(id: String, label: String, content: String) = info(id, label) { content }
-        @JvmStatic fun info(id: String, label: String, supplier: Supplier<String>) = info(id, label, supplier::get)
-        @JvmStatic fun info(id: String, label: String, supplier: () -> String) = from(id, label) { StatusReport(StatusReport.Priority.INFO, supplier()) }
+        @JvmStatic fun info(id: String, label: String, supplier: Supplier<out String>) = info(id, label, supplier::get)
+        @JvmStatic fun info(id: String, label: String, supplier: () -> String) =
+                StatusComponent(id, label) { StatusReport( StatusReport.Priority.INFO, supplier()) }
 
-        @JvmStatic fun from(id: String, label: String, content: StatusReport) = from(id, label) { content }
-        @JvmStatic fun from(id: String, label: String, supplier: Supplier<StatusReport>) = from(id, label, supplier::get)
-        @JvmStatic fun from(id: String, label: String, supplier: () -> StatusReport) = object : StatusComponent(id, label) {
-            override fun report(): StatusReport = supplier()
-        }
+        @JvmStatic fun from(id: String, label: String, content: StatusReport) = StatusComponent(id, label) { content }
+        @JvmStatic fun from(id: String, label: String, supplier: Supplier<out StatusReport>) = StatusComponent(id, label, supplier::get)
+        @JvmStatic fun from(id: String, label: String, supplier: () -> StatusReport) = StatusComponent(id, label, supplier)
     }
 }
