b2f95c102122e78134b57f5f729269fe4a4b4899
Steve Haslam <araqnid@googlemail.com>
2020-02-17 16:53:44 +0000

    Prune unused and dubious readyWhen

--- core/src/main/kotlin/org/araqnid/appstatus/Readiness.kt
+++ core/src/main/kotlin/org/araqnid/appstatus/Readiness.kt
@@ -1,9 +1,5 @@
 package org.araqnid.appstatus
 
 enum class Readiness {
-    READY, NOT_READY;
-
-    companion object {
-        fun readyWhen(predicate: () -> Boolean) = if (predicate()) READY else NOT_READY
-    }
+    READY, NOT_READY
 }
