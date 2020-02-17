f112d33a2565f4d87fb5728a64523503dd8937fc
Steve Haslam <araqnid@googlemail.com>
2020-02-17 17:32:42 +0000

    Reformat

--- core/src/main/kotlin/org/araqnid/appstatus/StatusComponent.kt
+++ core/src/main/kotlin/org/araqnid/appstatus/StatusComponent.kt
@@ -1,6 +1,8 @@
 package org.araqnid.appstatus
 
-class StatusComponent(val id: String, val label: String, private val supplier: () -> StatusReport) : ReportConfigurable<StatusComponent> {
+class StatusComponent(val id: String,
+                      val label: String,
+                      private val supplier: () -> StatusReport) : ReportConfigurable<StatusComponent> {
     fun report(): StatusReport = supplier()
 
     fun mapReport(fn: (StatusReport) -> StatusReport) =
@@ -8,17 +10,33 @@ class StatusComponent(val id: String, val label: String, private val supplier: (
 
     override fun mapText(fn: (String) -> String): StatusComponent =
             mapReport { it.mapText(fn) }
+
     override fun limitPriority(maxPriority: StatusReport.Priority): StatusComponent =
             mapReport { it.limitPriority(maxPriority) }
 
     companion object {
-        @JvmStatic fun info(id: String, label: String, content: String) = info(id, label) { content }
-        @JvmStatic fun info(id: String, label: String, supplier: java.util.function.Supplier<String>) = info(id, label, supplier::get)
-        @JvmStatic fun info(id: String, label: String, supplier: () -> String) =
-                StatusComponent(id, label) { StatusReport( StatusReport.Priority.INFO, supplier()) }
+        @JvmStatic
+        fun info(id: String, label: String, content: String) =
+                info(id, label) { content }
 
-        @JvmStatic fun from(id: String, label: String, content: StatusReport) = StatusComponent(id, label) { content }
-        @JvmStatic fun from(id: String, label: String, supplier: java.util.function.Supplier<StatusReport>) = StatusComponent(id, label, supplier::get)
-        @JvmStatic fun from(id: String, label: String, supplier: () -> StatusReport) = StatusComponent(id, label, supplier)
+        @JvmStatic
+        fun info(id: String, label: String, supplier: java.util.function.Supplier<String>) =
+                info(id, label, supplier::get)
+
+        @JvmStatic
+        fun info(id: String, label: String, supplier: () -> String) =
+                StatusComponent(id, label) { StatusReport(StatusReport.Priority.INFO, supplier()) }
+
+        @JvmStatic
+        fun from(id: String, label: String, content: StatusReport) =
+                StatusComponent(id, label) { content }
+
+        @JvmStatic
+        fun from(id: String, label: String, supplier: java.util.function.Supplier<StatusReport>) =
+                StatusComponent(id, label, supplier::get)
+
+        @JvmStatic
+        fun from(id: String, label: String, supplier: () -> StatusReport) =
+                StatusComponent(id, label, supplier)
     }
 }
